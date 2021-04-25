af424c43b081541f7de6b37aa14af16da2484413
jihooon <baramofme@gmail.com>
2021-04-25 12:48:05 +0900

    Vuetify admin 무료 템플릿으로 소스 교체

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,9 @@
 <template>
- <div id="app">
-  <v-app id="inspire">
-    <v-app id="inspire">
-      <v-navigation-drawer
-        v-model="drawer"
-        app color="purple lighten-3"
-      >
-        <v-list dense>
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Home</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon>mdi-email</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Contact</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-  
-      <v-app-bar
-        app
-        color="red"
-        dark
-      >
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title class="font-italic">이것은 무엇에</v-toolbar-title>
-      </v-app-bar>
-  
-      <v-main>
-        <v-container
-          class="fill-height"
-          fluid
-        >
-          <v-row
-            align="center"
-            justify="center"
-          >
-            <v-col class="text-center">
-              <v-tooltip left>
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    :href="source"
-                    icon
-                    large
-                    target="_blank"
-                    v-on="on"
-                  >
-                    <v-icon large>mdi-code-tags</v-icon>
-                  </v-btn>
-                </template>
-                <span>Source</span>
-              </v-tooltip>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-      <v-footer
-        color="indigo"
-        app
-      >
-        <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
-      </v-footer>
-    </v-app>
-  </v-app>
-</div>
+  <router-view />
 </template>
 
 <script>
-
-
-export default {
-   el: '#app',
-  props: {
-    source: String,
-  },
-
-  data: () => ({
-    drawer: true,
-  }),
-};
+  export default {
+    name: 'App',
+  }
 </script>
