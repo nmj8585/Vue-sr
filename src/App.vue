3b478199808b78b1fe81d1c010a0254124e952bf
nmj8585 <nmj8585@gmail.com>
2021-04-25 19:04:06 +0900

    #10-1  vuetify  버전 업
    [INITIAL] — 최초에 파일을 업로드 할 때
    [FIX] — vuetify 재설치
    
    우선 영수가 올려준걸 그냥 병합으로 설치해서 다시 해놨고
    하고나서 실행하니 위에 말해준 오류가 뜨진 않았으나
    vuetify 버전이 써있는 문구가 있어서
    일단 재설치하니 실행은 되었음 .

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,55 @@
 <template>
- <div id="app">
-  <v-app id="inspire">
-    <v-app id="inspire">
-      <v-navigation-drawer
-        v-model="drawer"
-        right="right"
-        height="50%"
-        app color="#3e2723"
-        light="true"
-        value="false"
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer />
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
       >
-        <v-list dense>
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Home</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-list-item link>
-            <v-list-item-action>
-              <v-icon>mdi-email</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>Contact</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-  
-      <v-app-bar
-        app
-        color="rgba(115, 175, 123, 1)"
-        dark
-      >
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-toolbar-title class="font-italic">이것은 무엇에</v-toolbar-title>
-      </v-app-bar>
-  
-      <v-main>
-        <v-container
-          class="fill-height"
-          fluid
-        >
-          <v-row
-            align="center"
-            justify="center"
-          >
-            <v-col class="text-center">
-              <v-tooltip left>
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    :href="source"
-                    icon
-                    large
-                    target="_blank"
-                    v-on="on"
-                  >
-                    <v-icon large>mdi-code-tags</v-icon>
-                  </v-btn>
-                </template>
-                <span>Source</span>
-              </v-tooltip>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-      <v-footer
-        color="indigo"
-        app
-      >
-        <span class="white--text">&copy; {{ new Date().getFullYear() }}</span>
-      </v-footer>
-    </v-app>
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
   </v-app>
-</div>
 </template>
 
 <script>
+
   export default {
     name: 'App',
+
+    data: () => ({
+    //
+    }),
   }
 </script>
