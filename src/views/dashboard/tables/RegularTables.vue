3a9c6407522b498cd6babe0f34e038653b258cd6
nmj8585 <nmj8585@gmail.com>
2021-05-16 08:55:33 +0900

    대시보드 3단 수정 연습 - 슬롯적용 연습중.

--- src/views/dashboard/tables/RegularTables.vue
+++ src/views/dashboard/tables/RegularTables.vue
@@ -69,8 +69,7 @@
       />
     </base-material-card>
     <base-material-card
-      color="warning"
-
+      color="pink"
       icon="mdi-clipboard-plus"
       title="Table on Dark Background"
       class="px-5 py-3"
@@ -82,20 +81,82 @@
         class="elevation-1"
       />
     </base-material-card>
+    <base-material-card
+      color="deep-purple darken-4"
+      cyan
+      icon="mdi-clipboard-plus"
+      title="이름가격도시연봉"
+      class="px-5 py-3"
+    >
+      <v-data-table
+        v-slot:items
+        :headers="headers"
+        :items="tableData"
+        :items-per-page="6"
+        class="elevation-1"
+      />
+      <template v-slot:items="{ items }">
+        <tr>
+          <td>{{ items.id }}</td>
+        </tr>
+      </template>
+    </base-material-card>
   </v-container>
 </template>
 <script>
   export default {
     data () {
       return {
-        tableHead: { id: 'ID', name: 'Name', country: 'Country', city: 'City', salary: 'Salary' },
+        tableHead: {
+          id: 'ID',
+          name: 'Name',
+          country: 'Country',
+          city: 'City',
+          salary: 'Salary',
+        },
         tableData: [
-          { id: 1, name: 'Dakota Rice', country: 'Niger', city: 'Oud-Turnhout', salary: '$36,738' },
-          { id: 2, name: 'Minverva Hooper', country: 'Curaçao', city: 'Sinaas-Waas', salary: '$23,789' },
-          { id: 3, name: 'Sage Rodriguez', country: 'Netherlands', city: 'Baileux', salary: '$56,142' },
-          { id: 4, name: 'Philip Chaney', country: 'Korea', city: 'South 서울', salary: '$38,735' },
-          { id: 5, name: 'Doris Greene', country: 'Malawi', city: 'Feldkirchen in Kärnten', salary: '$63,542' },
-          { id: 6, name: 'Mason Porter', country: 'Chile', city: 'Gloucester', salary: '$78,615' },
+          {
+            id: 1,
+            name: 'Dakota Rice',
+            country: 'Niger',
+            city: 'Oud-Turnhout',
+            salary: '$36,738',
+          },
+          {
+            id: 2,
+            name: 'Minverva Hooper',
+            country: 'Curaçao',
+            city: 'Sinaas-Waas',
+            salary: '$23,789',
+          },
+          {
+            id: 3,
+            name: 'Sage Rodriguez',
+            country: 'Netherlands',
+            city: 'Baileux',
+            salary: '$56,142',
+          },
+          {
+            id: 4,
+            name: 'Philip Chaney',
+            country: 'Korea',
+            city: 'South 서울',
+            salary: '$38,735',
+          },
+          {
+            id: 5,
+            name: 'Doris Greene',
+            country: 'Malawi',
+            city: 'Feldkirchen in Kärnten',
+            salary: '$63,542',
+          },
+          {
+            id: 6,
+            name: 'Mason Porter',
+            country: 'Chile',
+            city: 'Gloucester',
+            salary: '$78,615',
+          },
         ],
         headers: [
           {
@@ -104,10 +165,22 @@
             sortable: false,
             value: 'id',
           },
-          { text: 'Name', value: 'name' },
-          { text: 'Country', value: 'country' },
-          { text: 'City', value: 'city' },
-          { text: 'Salary', value: 'salary' },
+          {
+            text: 'Name',
+            value: 'name',
+          },
+          {
+            text: 'Country',
+            value: 'country',
+          },
+          {
+            text: 'City',
+            value: 'city',
+          },
+          {
+            text: 'Salary',
+            value: 'salary',
+          },
         ],
 
       }
